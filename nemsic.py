9f34067786c83dc3a86ea6507d34a4438d8217b3
BeastWithin <BeastWithin@users.noreply.github.com>
2021-07-31 21:57:40 +0300

    Update nemsic.py
    DHT den veri alma, sıcaklık 25 ten fazlaysa alarm yollama, 10 dk da bir ölçüm yapma

--- nemsic.py
+++ nemsic.py
@@ -1,51 +1,59 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import sys
+import os
+import re
+import time
+from smtplib import SMTP                  # use this for standard SMTP protocol   (port 25, no encryption)
+import adafruit_dht
+from email.mime.text import MIMEText
 
 
 SMTPserver = 'smtp.office365.com'
 port =587
 sender =     'nemsic@hotmail.com'
-destination = ['cengizhasan@gmail.com']
-
+destination = ['eczsinancengiz@gmail.com']
+#destination = ['eczpinar.ekmek@gmail.com']
 USERNAME = "nemsic@hotmail.com"
 PASSWORD = "Y3QLt66TLGRkxNt"
-
 # typical values for text_subtype are plain, html, xml
 text_subtype = 'plain'
+subject="NemSıc Alarm"
+lastAlarmSent=""
+
+#DHT işlemleri
+def get_data(sensorpin):
+    dhtDevice = adafruit_dht.DHT11(sensorpin)
+    sıc=dhtDevice.temperature
+    nem=dhtDevice.humidity
+    return (sıc,nem)
 
 
-content="""\
-Test message
-"""
-
-subject="Sent from Python"
-
-import sys
-import os
-import re
-
-#from smtplib import SMTP_SSL as SMTP       # this invokes the secure SMTP protocol (port 465, uses SSL)
-from smtplib import SMTP                  # use this for standard SMTP protocol   (port 25, no encryption)
-
-# old version
-# from email.MIMEText import MIMEText
-from email.mime.text import MIMEText
-
-try:
+def sendalarm(sıc,nem):
+    content="\n{}\nSıcaklık:{}°C\nNem:%{}".format(str(time.ctime()),sıc,nem)
     msg = MIMEText(content, text_subtype)
     msg['Subject']=       subject
     msg['From']   = sender # some SMTP servers will do this automatically, not all
-
+    
     conn = SMTP(SMTPserver,port)
     conn.set_debuglevel(1)
     conn.ehlo()
     conn.starttls()
     conn.login(USERNAME, PASSWORD)
-    try:
-        print("{} tarafından {} adresine {} konulu mesaj yollanıyor...",format(sender,destination,subject))
-        conn.sendmail(sender, destination, msg.as_string())
-    finally:
-        conn.quit()
+    #try:
+        #print("{} tarafından {} adresine {} konulu mesaj yollanıyor...",format(sender,destination,subject))
+    conn.sendmail(sender, destination, msg.as_string())
+    #except:
+        #print("Başaramadık abi")
+    #finally:
+    conn.quit()
 
-except:
-    sys.exit( "mail failed; %s" % "CUSTOM_ERROR" ) # give an error message
+while True:
+    sıc,nem=get_data(23)
+    os.system("echo {},{},{} >> '{}.txt'".format(time.strftime("%H:%M:%S"),sıc,nem,time.strftime("%Y %m")))
+    #pyexcel_ods.write_data(str(time.strftime("%Y %m"))+" data.ods",{time.strftime("%d"):[["Saat",time.strftime("%H:%M:%S")],["Sıcaklık",2],["Nem",2]]})
+    if sıc > 25:
+        sendalarm(sıc,nem)
+    time.sleep(600)
+
+
