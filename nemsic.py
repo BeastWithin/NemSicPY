9bf2ab2e0285843effb47ab030f20220ebed8287
BeastWithin <BeastWithin@users.noreply.github.com>
2021-08-01 14:50:46 +0300

    Update nemsic.py
    sensor okunamadı arızası düzeldi. dhtdevice.exit eklendi.

--- nemsic.py
+++ nemsic.py
@@ -20,12 +20,26 @@ PASSWORD = "Y3QLt66TLGRkxNt"
 text_subtype = 'plain'
 subject="NemSıc Alarm"
 lastAlarmSent=""
+sys.stdout=open("NemSıc.log","a")
 
 #DHT işlemleri
 def get_data(sensorpin):
-    dhtDevice = adafruit_dht.DHT11(sensorpin)
-    sıc=dhtDevice.temperature
-    nem=dhtDevice.humidity
+    den=0
+    sıc=None
+    nem=None
+    while den<25:
+        try: #25 defa sensor okumayı denesin diye
+            dhtDevice = adafruit_dht.DHT11(sensorpin)
+            sıc=dhtDevice.temperature
+            nem=dhtDevice.humidity
+            dhtDevice.exit() #sensorün yine okunabilmesi için şart. yoksa ligpio işlemi yüzünden hata veriyor. 
+            break
+        except:
+            dhtDevice.exit()
+            den+=1
+            print("Sensor okunamadı: Deneme {}".format(den))
+            time.sleep(0.77)
+            continue
     return (sıc,nem)
 
 
@@ -34,15 +48,25 @@ def sendalarm(sıc,nem):
     msg = MIMEText(content, text_subtype)
     msg['Subject']=       subject
     msg['From']   = sender # some SMTP servers will do this automatically, not all
-    
-    conn = SMTP(SMTPserver,port)
-    conn.set_debuglevel(1)
+    try:
+        conn = SMTP(SMTPserver,port)
+    except:
+        print("Email sunucusuna bağlanılamadı.")
+        return
+    #conn.set_debuglevel(1)
     conn.ehlo()
     conn.starttls()
-    conn.login(USERNAME, PASSWORD)
+    try:
+        conn.login(USERNAME, PASSWORD)
+    except:
+        print("Login Hatası")
+        return
     #try:
         #print("{} tarafından {} adresine {} konulu mesaj yollanıyor...",format(sender,destination,subject))
-    conn.sendmail(sender, destination, msg.as_string())
+    try:
+        conn.sendmail(sender, destination, msg.as_string())
+    except:
+        print("Email gönderimi başarısız")
     #except:
         #print("Başaramadık abi")
     #finally:
@@ -50,10 +74,13 @@ def sendalarm(sıc,nem):
 
 while True:
     sıc,nem=get_data(23)
-    os.system("echo {},{},{} >> '{}.txt'".format(time.strftime("%H:%M:%S"),sıc,nem,time.strftime("%Y %m")))
+    os.system("echo {},{},{} >> '{}.txt'".format(time.strftime("%H:%M:%S"),sıc,nem,time.strftime("%Y %m"))) #txt dosyasına verileri kaydetme
     #pyexcel_ods.write_data(str(time.strftime("%Y %m"))+" data.ods",{time.strftime("%d"):[["Saat",time.strftime("%H:%M:%S")],["Sıcaklık",2],["Nem",2]]})
-    if sıc > 25:
+    if sıc > 25 or sıc==None:
         sendalarm(sıc,nem)
     time.sleep(600)
+
+
+sys.stdout.close()
 
 
